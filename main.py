19aad3d79cd21dfb364017778b68fcfa9965ebb0
sarakmenon <107772944+sarakmenon@users.noreply.github.com>
2025-01-09 11:26:22 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from turtle import Screen
+from turtle import Screen, Turtle
 from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
@@ -8,7 +8,34 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
 
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(player.move_up, "Up")
+
+level = 1
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    car_manager.create_car()
+    car_manager.move_cars()
+
+
+    if car_manager.check_collision(player):
+        player.color("red")
+        game_is_on = False
+        scoreboard.game_over()
+
+    if player.ycor() >= 290:
+        scoreboard.increase_level()
+        scoreboard.update_scoreboard()
+        car_manager.increase_speed()
+        player.sety(-290)
+
+
+screen.exitonclick()
